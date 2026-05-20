fbaf90eda5f9ed2a3966f341fa80d104b9c39d38
raajkumar <raajkumardevops@gmail.com>
2026-05-21 02:33:37 +0530

    Connected frontend to live backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mini-saas-backend-y354.onrender.comi",
 })
 
 export default API
