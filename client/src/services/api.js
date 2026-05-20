abb1a6c1c79e0122d67fe44840ff88d22e5399ea
raajkumar <raajkumardevops@gmail.com>
2026-05-21 02:41:33 +0530

    api connected in api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://mini-saas-backend-y354.onrender.com",
+  baseURL: "https://mini-saas-backend-y354.onrender.com/api",
 })
 
 export default API
