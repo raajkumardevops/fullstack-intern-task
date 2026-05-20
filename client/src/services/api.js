51e5ba410e221241ea29cf9a43b3c1bd84f5a5b7
raajkumar <raajkumardevops@gmail.com>
2026-05-21 02:36:46 +0530

    problem solved in api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "https://mini-saas-backend-y354.onrender.comi",
+  baseURL: "https://mini-saas-backend-y354.onrender.com",
 })
 
 export default API
